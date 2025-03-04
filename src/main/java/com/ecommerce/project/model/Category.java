668928e252bd8715087e19708ccdaadc9855c551
Sarthak <saryatnam22@gmail.com>
2025-03-04 15:25:38 +0530

    marked an enity via JPA

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,12 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")     // an entity in JPA means, a table in DB. This particular class will be mapped to a table in DB by spring, and the table will contain attributes of the class members. The name of the entity is "categories"
 public class Category {
+
+    @Id     // means this attribute will be the primary key of the table
     private Long categoryId;
     private String categoryName;
 
