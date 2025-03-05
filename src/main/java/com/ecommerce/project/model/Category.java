9b894e26622e057e2dd97d205add89b80e84862a
Sarthak <saryatnam22@gmail.com>
2025-03-05 15:44:05 +0530

    defined Generation.Type for identity for Id

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,21 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity(name = "categories")     // an entity in JPA means, a table in DB. This particular class will be mapped to a table in DB by spring, and the table will contain attributes of the class members. The name of the entity is "categories"
 public class Category {
 
-    @Id     // means this attribute will be the primary key of the table
+    // means this attribute will be the primary key of the table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     //specifies how primary key values should be generated. Has values auto, identity, sequence, table, none
+
+//    a "sequence" is an object of db that resides in the db, which generates sequence of unique values. allocationSize = 1 means sequence increment it by 1
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = 'order_seq')
+//    @SequenceGenerator(name = 'order_seq', sequenceName = 'order_sequence', allocationSize = 1)
+
+//    if db doesnt support sequence, table can be used
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = 'table_gen')
+//    @TableGenerator(name = 'table_gen', table = 'id_gen', pkColumnName = 'gen_key', valueColumnName = 'gen_value', pkColumnValue = 'task_id', allocationSize = 1)
     private Long categoryId;
     private String categoryName;
 
@@ -29,5 +38,10 @@ public class Category {
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+//    good practice to have a default constructor in JPA
+    public Category() {
+
     }
 }
