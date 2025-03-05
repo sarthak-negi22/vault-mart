f645b6dd66cdb31d1831d1e728354149222ca3c5
Sarthak <saryatnam22@gmail.com>
2025-03-05 20:45:43 +0530

    made the category persistent

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.ecommerce.project.repositories;
 import com.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// JPA repo provides built-im methods for CRUD operations, custom queries.
 // JpaRespsitory parameters require Entity type, primary key type
+// spring automatically generates SQL queries based on methods name
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImplementation.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImplementation.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,33 +13,38 @@ import java.util.Optional;
 
 @Service            // tells spring that it needs to manage this class as a bean
 public class CategoryServiceImplementation implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
+//    private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;       // L specifies that its a long literal, not to be confused with int
+
+    @Autowired      // Tells spring  to automatically inject instance of this type, automatically searches  for a bean of the type CategoryRepository and assigns it to the object
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found!"));      //if not found, throw an exception of this type.
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId: "+categoryId+" is deleted!";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Optional<Category> optionalCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst();
@@ -45,7 +52,8 @@ public class CategoryServiceImplementation implements CategoryService {
         if(optionalCategory.isPresent()) {
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            Category savedCategory = categoryRepository.save(existingCategory);
+            return savedCategory;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found!");
         }
