5aa0cde6671d5c77279beb213afb2a9c4ad65e83
Sarthak <saryatnam22@gmail.com>
2025-03-10 23:36:54 +0530

    implemented custom APIException for not allowing creation of same name categories

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRespsitory parameters require Entity type, primary key type
 // spring automatically generates SQL queries based on methods name
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    Category findByCategoryName(String categoryName);   // spring analyze the method name, and generate the desired SQL corresponding to the method name, without us re-writing the complex SQL
 }
