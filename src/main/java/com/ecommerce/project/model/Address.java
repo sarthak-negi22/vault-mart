6b9cf21c8bf0e57e4038d82c0a9dad61147ff8ca
Sarthak <saryatnam22@gmail.com>
2025-05-14 20:21:22 +0530

    refactor: fix some loose ends

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +47,7 @@ public class Address {
     private String pincode;
 
     @ManyToMany(mappedBy = "addresses")
+    @ToString.Exclude       // this field won't be displayed in toString() method of this class
     private List<User> users = new ArrayList<>();
 
     public Address(String street, String buildingName, String city, String state, String country, String pincode) {
